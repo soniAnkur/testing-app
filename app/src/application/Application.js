19fbe275424baa6c3960f937733308d67940c28f
ankursoni <me.soni88@gmail.com>
2016-02-08 23:31:27 +0530

    refactorings for route  sidebar  +  refactoring for ui-view one lavel parent

--- app/src/application/Application.js
+++ app/src/application/Application.js
@@ -2,7 +2,25 @@
   'use strict';
 
   // Prepare the 'users' module for subsequent registration of controllers and delegates
-  angular.module('application', [ 'ngMaterial' ]);
+  var application = angular.module('application', [ 'ui.router',
+                                                    'ngMaterial' ]);
+    application.config(function ($stateProvider, $urlRouterProvider) {
 
+        //$urlRouterProvider.when('/dashboard', '/dashboard/overview');
+        $urlRouterProvider.otherwise('/sidebar');
+
+        $stateProvider
+            .state('base', {
+                abstract: true,
+                url: '',
+                templateUrl: 'src/application/view/base.html'
+            })
+            .state('sidebar', {
+                url: '/sidebar',
+                parent: 'base',
+                templateUrl: 'src/application/view/sidebar.html'
+                //controller: 'DashboardCtrl'
+            })
+    });
 
 })();
